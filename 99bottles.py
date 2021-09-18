77d05e186fa88432ab2112bf4d0043e407f7e4f2
s0yun <soyun.sa@hotmail.com>
2021-09-18 12:08:10 +0100

    gramatical changes

--- 99bottles.py
+++ 99bottles.py
@@ -1,11 +1,13 @@
-print("99 bottles of beer on the wall, 99 bottles of beer")
-print ("take one down, pass it around")
-bottles = 99
-while bottles != 1:
-  print(str (bottles) + " bottles of beer on the wall, " + str(bottles) + " bottles")
-  bottles -= 1
-  print ("take one down, pass it around")
-print(str (bottles) + " bottle of beer on the wall")
-print ("take it down, pass it around")
-print ("No more bottles of beer on the wall, no more bottles of beer.")
-print ("Go to the store and buy some more, 99 bottles of beer on the wall.")
+numbers = "zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen twenty twenty-one twenty-two twenty-three twenty-four twenty-five twenty-six twenty-seven twenty-eight twenty-nine thirty thirty-one thirty-two thirty-three thirty-four thirty-five thirty-six thirty-seven thirty-eight thirty-nine forty forty-one forty-two forty-three forty-four forty-five forty-six forty-seven forty-eight forty-nine fifty fifty-one fifty-two fifty-three fifty-four fifty-five fifty-six fifty-seven fifty-eight fifty-nine sixty sixty-one sixty-two sixty-three sixty-four sixty-five sixty-six sixty-seven sixty-eight sixty-nine seventy seventy-one seventy-two seventy-three seventy-four seventy-five seventy-six seventy-seven seventy-eight seventy-nine eighty eighty-one eighty-two eighty-three eighty-four eighty-five eighty-six eighty-seven eighty-eight eighty-nine ninety ninety-one ninety-two ninety-three ninety-four ninety-five ninety-six ninety-seven ninety-eight ninety-nine"
+x = numbers.split()
+x = list(x)
+for i in reversed(x):
+    if i == 'one':
+      print(i, "bottle of beer on the wall", i, "bottle of beer on the wall")
+    elif i == 'zero':
+      print ("No more bottles of beer on the wall, no more bottles of beer.")
+      print ("Go to the store and buy some more, 99 bottles of beer on the wall.")
+    else:
+      print(i, "bottles of beer on the wall", i, "bottles of beer on the wall")
+      print("take it down, pass it around")
+
