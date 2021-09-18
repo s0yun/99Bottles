282874db477998387a7544053224a7aae42f8f40
s0yun <soyun.sa@hotmail.com>
2021-09-18 10:17:49 +0100

    initial commit

---
+++ 99bottles.py
@@ -0,0 +1,11 @@
+print("99 bottles of beer on the wall, 99 bottles of beer")
+print ("take one down, pass it around")
+bottles = 99
+while bottles != 1:
+  print(str (bottles) + " bottles of beer on the wall, " + str(bottles) + " bottles")
+  bottles -= 1
+  print ("take one down, pass it around")
+print(str (bottles) + " bottle of beer on the wall")
+print ("take it down, pass it around")
+print ("No more bottles of beer on the wall, no more bottles of beer.")
+print ("Go to the store and buy some more, 99 bottles of beer on the wall.")
